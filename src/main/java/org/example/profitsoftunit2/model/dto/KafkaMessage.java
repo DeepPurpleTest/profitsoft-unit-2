8dc920a4eb18c6fc31a52414aad4a8e48aef6546
Moksem <maks.viskovatov@gmail.com>
2024-06-04 14:08:13 +0300

    setup kafka

---
+++ src/main/java/org/example/profitsoftunit2/model/dto/KafkaMessage.java
@@ -0,0 +1,15 @@
+package org.example.profitsoftunit2.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KafkaMessage {
+
+	private String message;
+}
