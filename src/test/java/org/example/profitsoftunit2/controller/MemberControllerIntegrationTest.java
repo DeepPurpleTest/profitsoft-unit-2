58f6e13ecdf249b169cfc80c3d78d7aead9598ba
Moksem <maks.viskovatov@gmail.com>
2024-06-11 20:30:07 +0300

    fix tests

--- src/test/java/org/example/profitsoftunit2/controller/MemberControllerIntegrationTest.java
+++ src/test/java/org/example/profitsoftunit2/controller/MemberControllerIntegrationTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -27,6 +29,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class MemberControllerIntegrationTest {
 
 	@Autowired
